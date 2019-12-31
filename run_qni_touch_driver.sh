027490121ef5b20ec00ebaa43969d97f70045284
arduino12 <arduino12@users.noreply.github.com>
2019-12-31 17:45:36 +0200

    Add led reset while calibrate sensors!

--- run_qni_touch_driver.sh
+++ run_qni_touch_driver.sh
@@ -8,10 +8,11 @@ fi
 
 #-> Make sure the driver is not running yet
 QNI_TOUCH_DRIVER_BIN=./src/qni_touch_driver.py
-if pgrep -f $QNI_TOUCH_DRIVER_BIN > /dev/null; then
-	echo 'Please kill previous touch driver!' 1>&2
-	exit 2
-fi
+pkill -f $QNI_TOUCH_DRIVER_BIN
+# if pgrep -f $QNI_TOUCH_DRIVER_BIN > /dev/null; then
+	# echo 'Please kill previous touch driver!' 1>&2
+	# exit 2
+# fi
 
 #-> Go to the directory of this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
--- src/qni_touch_driver.py
+++ src/qni_touch_driver.py
@@ -2,6 +2,7 @@
 
 import time
 import logging
+import RPi.GPIO as GPIO
 import capacitive_electrodes
 
 from qni_core import logger, config
@@ -10,12 +11,17 @@ from qni_core import logger, config
 class QniTouchDriver(object):
     _logger = logging.getLogger('qni_touch_driver')
     UPDATE_DELAY = 0.1
+    LED_ENABLE_PIN = 4
 
     def __init__(self):
         logger.config_logger()
         self.electrodes = capacitive_electrodes.CapacitiveElectrodes(
             config.ELECTRODES_SIZE, config.TILE_SIZE, config.MPR121_LAYOUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.LED_ENABLE_PIN, GPIO.OUT)
+        GPIO.output(self.LED_ENABLE_PIN, GPIO.LOW)
         self.electrodes.init()
+        GPIO.setup(self.LED_ENABLE_PIN, GPIO.IN)
 
     def mainloop(self):
         while True:
